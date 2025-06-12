1a4828a65b14e9aba4517dc3d99df06134c9c561
conquestace <ashiful@conquestace.com>
2025-06-12 05:43:22 -0400

    test

--- src/components/footer-logic/InteractiveBookshelf.jsx
+++ src/components/footer-logic/InteractiveBookshelf.jsx
@@ -55,7 +55,7 @@ export default function InteractiveBookshelf() {
 
   return (
     <DndProvider backend={HTML5Backend}>
-      <div className="relative w-full min-h-[400px] bg-[url('/assets/floor-texture.svg')] bg-repeat rounded-t-xl overflow-hidden">
+      <div className="relative w-full min-h-[400px] bg-[url('/assets/footer/graphpaper.svg')] bg-repeat rounded-t-xl overflow-hidden">
 
         {/* decorative rectangles */}
         {randomRects.map((r, i) => (
